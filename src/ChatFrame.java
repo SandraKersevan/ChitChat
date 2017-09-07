c3e4ec319b40107300e14cc52e134651065c4aab
Sandra Kerševan <sandra.kersevan@gmail.com>
2017-09-08 01:10:51 +0200

    dodala datoteke

---
+++ src/ChatFrame.java
@@ -0,0 +1,261 @@
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
+
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.net.URISyntaxException;
+
+import javax.swing.JRadioButton;
+
+@SuppressWarnings("serial")
+public class ChatFrame extends JFrame implements ActionListener, KeyListener {
+	
+	private JTextArea output;
+	private JTextField input;
+	public JTextField userName;
+	private USER u;
+	
+	boolean prijava;
+		
+	public ChatFrame() {
+		super();
+		setTitle("ChitChat");
+		Container pane = this.getContentPane();
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0};
+		gridBagLayout.columnWeights = new double[]{0.0};
+		pane.setLayout(gridBagLayout);
+
+		this.output = new JTextArea(20, 40);
+		this.output.setEditable(false);
+		GridBagConstraints outputConstraint = new GridBagConstraints();
+		outputConstraint.insets = new Insets(0, 0, 5, 0);
+		outputConstraint.gridx = 0;
+		outputConstraint.gridy = 1;
+		outputConstraint.fill = GridBagConstraints.BOTH;
+		outputConstraint.weightx = 1;
+		outputConstraint.weighty = 1;
+		
+		JScrollPane scrollpane = new JScrollPane(this.output,
+				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		pane.add(scrollpane, outputConstraint);
+		
+		JPanel panel_2 = new JPanel();
+		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
+		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_2.fill = GridBagConstraints.BOTH;
+		gbc_panel_2.gridx = 0;
+		gbc_panel_2.gridy = 2;
+		getContentPane().add(panel_2, gbc_panel_2);
+		
+		JRadioButton rdbtnJavno = new JRadioButton("JAVNO");
+		rdbtnJavno.setSelected(true);
+		panel_2.add(rdbtnJavno);
+		
+		JRadioButton rdbtnPrivat = new JRadioButton("PRIVAT");
+		panel_2.add(rdbtnPrivat);
+		
+		JTextField privatUporabnik = new JTextField();
+		privatUporabnik.setEnabled(false);
+		panel_2.add(privatUporabnik);
+		privatUporabnik.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.anchor = GridBagConstraints.SOUTH;
+		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 3;
+		getContentPane().add(panel_1, gbc_panel_1);
+		
+		JLabel lblSporocilo = new JLabel("Sporoèilo:");
+		panel_1.add(lblSporocilo);
+		
+		this.input = new JTextField(40);
+		panel_1.add(input);
+		
+		JButton btnPoslji = new JButton("Pošlji");
+		btnPoslji.setEnabled(false);
+		panel_1.add(btnPoslji);
+		
+		input.addKeyListener(this);		
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		JLabel lblUporabniskoIme = new JLabel("Uporabniško ime:");
+		
+		this.userName = new JTextField(System.getProperty("user.name"), 20);
+		this.u = new USER(this, userName.getText().trim(), prijava);
+		this.u.activate();
+		this.u.run();
+		
+		panel.add(lblUporabniskoIme);
+		panel.add(userName);
+		
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.HORIZONTAL;
+		constraints.insets = new Insets(0, 0, 5, 0);
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		
+		pane.add(panel, constraints);
+		
+		JButton btnPrijava = new JButton("Prijava");
+		panel.add(btnPrijava);
+		
+		JButton btnOdjava = new JButton("Odjava");
+		btnOdjava.setEnabled(false);
+		panel.add(btnOdjava);
+		
+		JButton btnPrisotni = new JButton("Prisotni");
+		btnPrisotni.setEnabled(false);
+		panel.add(btnPrisotni);
+		
+		// gumb Prijava kljiknjen
+		btnPrijava.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				try {
+					u.user = userName.getText();
+					u.logIn(userName.getText().trim());
+				} catch (URISyntaxException e) {
+					e.printStackTrace();
+					System.out.println("Prišlo je do napake ob kliku na gumb Prijava.");
+				}
+				if (u.prijava){
+					btnOdjava.setEnabled(true);
+					btnPrisotni.setEnabled(true);
+					btnPrijava.setEnabled(false);
+					btnPoslji.setEnabled(true);
+				}
+			}
+		});
+		
+		// gumb Odjava kliknjen
+		btnOdjava.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					u.logOut(userName.getText().trim());
+				} catch (URISyntaxException e1) {
+					e1.printStackTrace();
+					System.out.println("Prišlo je do napake ob kliku na gumb Odjava.");
+				}
+				if (!u.prijava) {
+					btnOdjava.setEnabled(false);
+					btnPrisotni.setEnabled(false);
+					btnPrijava.setEnabled(true);
+					btnPoslji.setEnabled(false);
+				}
+			}
+		});
+		
+		// gumb Prisotni kliknjen
+		btnPrisotni.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				u.getUsers();
+			}
+		});
+		
+		// javno sporoèilo
+		rdbtnJavno.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				rdbtnPrivat.setSelected(false);
+				rdbtnJavno.setSelected(true);
+				privatUporabnik.setEnabled(false);
+				privatUporabnik.setText("");
+			}
+		});
+				
+		// privat sporoèilo
+		rdbtnPrivat.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				rdbtnJavno.setSelected(false);
+				privatUporabnik.setEnabled(true);
+				rdbtnPrivat.setSelected(true);
+			}
+		});
+		
+		// poslji sporoèilo
+		btnPoslji.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(rdbtnJavno.isSelected()) {
+					try {
+						u.sendMessage(userName.getText().trim(), "true", input.getText().trim(), "");
+					} catch (URISyntaxException e1) {
+						e1.printStackTrace();
+						System.out.println("Prišlo je do napake pri pošiljanju javnega sporoèila.");
+					} 
+				} else {
+					try {
+						u.sendMessage(userName.getText().trim(), "false", input.getText(), privatUporabnik.getText().trim());
+					} catch (URISyntaxException e1) {
+						e1.printStackTrace();
+						System.out.println("Prišlo je do napake pri pošiljanju privat sporoèila.");
+					} 
+				} 
+				input.setText("");
+			}
+		});
+	}
+
+	/**
+	 * @param person - the person sending the message
+	 * @param message - the message content
+	 */
+	public void addMessage(String person, String message) {
+		String chat = this.output.getText();
+		if ((person == "") && (message.length() > 2)) {
+			this.output.setText(chat + message + "\n");
+		} if ((person != "") && (message.length() > 2)) {
+			this.output.setText(chat + person + ": " + message + "\n");
+		}
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if (e.getSource() == this.input) {
+			if (e.getKeyChar() == '\n') {
+				this.addMessage(userName.getText(), this.input.getText());
+				this.input.setText("");
+			}
+		}		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/USER.java
@@ -0,0 +1,156 @@
+
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class USER extends TimerTask{
+	private ChatFrame chat;
+	public boolean prijava = false;
+	public String user;
+	
+	public USER(ChatFrame chat, String user, boolean prijava) {
+		this.chat = chat;
+		this.prijava = prijava;
+		this.user = user;
+	}
+
+	// robot
+	@Override
+	public void run(){
+		if (prijava) { 
+			try {
+				receiveMessage(this.user);
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+				System.out.println("Prišlo je do napake pri robotu.");
+			}
+		}
+	}
+
+	// ostali uporabniki
+	public void getUsers(){
+		try {
+			String hello = Request.Get("http://chitchat.andrej.com/users")
+						  .execute()
+						  .returnContent().asString();
+
+			chat.addMessage("","Trenutni so prisotni: " + hello);
+        } catch (IOException e) {
+            e.printStackTrace();
+			System.out.println("Prišlo je do napake pri pridobivanju prisotnih uporabnikov.");
+        }
+	}
+
+	// prijava
+	public void logIn(String user) throws URISyntaxException{
+		try {
+			String time = Long.toString(new Date().getTime());
+
+			URI uri = new URIBuilder("http://chitchat.andrej.com/users?username="+user)
+					.addParameter("stop_cache", time)
+					.build();
+
+			Request.Post(uri)
+					.execute()
+					.returnContent().asString();
+			
+			prijava = true;
+			chat.addMessage("", "Pozdravljeni " + user + "! Uspešno ste se prijavili!");
+        } catch (IOException e) {
+        	prijava = false;
+            e.printStackTrace();
+            chat.addMessage("", "Prišlo je do napake pri prijavi " + user + "!");
+            System.out.println("Prišlo je do napake pri prijavi.");
+        }
+	}	
+	
+	// odjava
+	public void logOut(String user) throws URISyntaxException{
+		try {
+			String time = Long.toString(new Date().getTime());
+
+			URI uri = new URIBuilder("http://chitchat.andrej.com/users?username="+user)
+					.addParameter("stop_cache", time)
+					.build();
+
+			Request.Delete(uri)
+					.execute()
+					.returnContent().asString();
+
+			prijava = false;
+			chat.addMessage("", "Nasvidenje " + user + "! Uspešno ste se odjavili!");
+        } catch (IOException e) {
+        	prijava = true;
+            e.printStackTrace();
+			System.out.println("Prišlo je do napake pri odjavi.");
+			chat.addMessage("", "Prišlo je do napake pri odjavi " + user + "!");
+        }
+	}
+	
+	/**
+	 * Activate the robot!
+	 */
+	public void activate() {
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(this, 5000, 1000);
+	}
+	
+	// prejeto sporoèilo
+	public void receiveMessage(String user) throws URISyntaxException{
+		try {
+			URI uri = new URIBuilder("http://chitchat.andrej.com/messages")
+			          .addParameter("username", user)
+			          .build();
+
+			String responseBody = Request.Get(uri)
+                            .execute()
+                            .returnContent().asString();	
+			
+			chat.addMessage(user, responseBody);		
+        } catch (IOException e) {
+            e.printStackTrace();
+			System.out.println("Prišlo je do napake pri prejemanju sporoèil.");
+        }
+	}
+	
+	// poslano sporoèilo
+	public void sendMessage(String user, String global, String text, String recipient) throws URISyntaxException{
+		try {
+			String time = Long.toString(new Date().getTime());
+
+			URI uri = new URIBuilder("http://chitchat.andrej.com/messages?username="+user)
+					.addParameter("stop_cache", time)
+					.build();
+			
+			String message = "{ \"global\" : "+global+", \"text\" : \""+text+"\"  }";
+			
+			// privat sporoèilo
+			if (global == "false") {
+				message = "{ \"global\" : "+global+", \"recipient\" : \""+recipient+"\", \"text\" : \""+text+"\"  }";
+			}
+			
+			String responseBody = Request.Post(uri)
+					.bodyString(message, ContentType.APPLICATION_JSON)
+					.execute()
+					.returnContent()
+					.asString();
+
+			if (global == "false") {
+				chat.addMessage("Za " + recipient, responseBody);
+			} else {
+				chat.addMessage(user, responseBody);
+			}
+        } catch (IOException e) {
+            e.printStackTrace();
+			System.out.println("Prišlo je do napake pri pošiljanju sporoèila.");
+			chat.addMessage("", "Prišlo je do napake pri pošiljanju sporoèila!");
+        }
+	}
+}
